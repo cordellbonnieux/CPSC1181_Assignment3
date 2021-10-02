97a152fa17c8f2772c74ffd28e6e2ac242e6c4eb
Cordell <cordellbonnieux@gmail.com>
2021-10-01 18:04:01 -0700

    Added the two required constructors and the needed getters and setters for the data members.

--- Message.java
+++ Message.java
@@ -1,8 +1,47 @@
 
 public class Message {
+	
+	public enum Status { READ, UNREAD, STARRED };
 	private String text;
 	private String sender;
 	private String recipient;
-	private enum Status {read, unread, starred};
+	private Status current;
+	
+	void Message(String t, String s, String r, Status x) {
+		this.text = t;
+		this.sender = s;
+		this.recipient = r;
+		this.current = x;
+	}
+	
+	void Message(String t, String s, String r) {
+		this.text = t;
+		this.sender = s;
+		this.recipient = r;
+		this.current = Status.UNREAD;
+	}
+	
+	public String getText() {
+		String t = this.text;
+		return t;
+	}
+	
+	public String getSender() {
+		String sender = this.sender;
+		return sender;
+	}
+	
+	public String getRecipient() {
+		String r = this.recipient;
+		return r;
+	}
+	
+	public Status getStatus() {
+		Status s = this.current;
+		return s;
+	}
+	public void setStatus(Status s) {
+		this.current = s;
+	}
 	
 }
