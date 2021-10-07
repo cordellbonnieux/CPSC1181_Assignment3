e5b83f4383f96b6523be6327a1674d38133524f5
Cordell <cordellbonnieux@gmail.com>
2021-10-07 11:49:53 -0700

    Added UserEntry abstract class

---
+++ UserEntry.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public abstract class UserEntry {
+	protected final Scanner scan; // add something
+	protected final String text; // add something
+	
+	public abstract String getUserResponse();
+}
